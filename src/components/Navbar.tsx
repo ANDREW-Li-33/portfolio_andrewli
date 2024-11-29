0a023cda7a0284c8c30a7a62970e356f7e169ee8
andrewli <andy.li3964@gmail.com>
2024-11-29 16:19:15 -0500

    make robot icon and my name clickable

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,12 @@ export default function Navbar(): JSX.Element {
   return (
     <nav className="p-4 bg-gray-800 text-white">
       <div className="container mx-auto flex justify-between">
-        <h1 className="text-lg font-bold flex items-center gap-2">
-          <Bot size={24} />
-          Andrew Li
-        </h1>
+        <a href="/">
+          <h1 className="text-lg font-bold flex items-center gap-2">
+            <Bot size={24} />
+            andrewli.info
+          </h1>
+        </a>
         <ul className="flex space-x-4 font-bold">
           <li>
             <a

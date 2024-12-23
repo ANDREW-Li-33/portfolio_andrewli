727fb42fa8bb10a3e216b8305772825f7640472f
andrewli <andy.li3964@gmail.com>
2024-12-23 13:57:10 -0500

    added custom landing page video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       {/* Video */}
       <video
         className="absolute top-0 left-0 w-full h-full object-cover"
-        src="/videos/short-term.mp4"
+        src="/videos/landing_page_video.mp4"
         autoPlay
         loop
         muted
@@ -14,13 +14,13 @@ export default function Home() {
       {/* Overlay */}
       <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-40">
         <h1 className="text-white text-4xl md:text-6xl font-bold mb-6">
-         Welcome to my portfolio! 
+         Welcome to my portfolio
         </h1>
         <a
           href="/projects"
           className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded-full transition-colors"
         >
-          View projects
+          View my projects!
         </a>
       </div>
     </div>

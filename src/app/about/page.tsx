b7d16eaa28a0832f2268f236ccd524c67d6f4999
andrewli <andy.li3964@gmail.com>
2025-02-11 17:57:52 -0500

    maintenance messages for about page and long term projects

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Construction} from 'lucide-react'
 
 export default function AboutPage(): JSX.Element {
   return (
-    <div className="container mx-auto p-6 text-center">
-      <h1 className="text-3xl font-bold mb-4">Page Under Maintenance!</h1>
+    <div className="bg-gray-800 rounded-lg mx-auto p-6 text-center mt-20 max-w-fit">
+      <h1 className="text-3xl font-bold mb-4 flex items-center justify-center gap-2 text-purple-300">
+        Page Under Maintenance!
+        <Construction size={36}/>
+      </h1>
       <p className="text-lg">
-        My about page is currently being updated, please come back on <strong>March 15, 2025</strong>.
+        My about page is currently being updated. Expected to come back up <span className='text-purple-300 font-bold'>February 18, 2025</span>.
       </p>
     </div>
   );
